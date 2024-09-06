f2b19cb60b3e5a9c162e7dd1c293e41afbdb780a
Diego Orellana Ávila <diego.orellanaa@ucuenca.edu.ec>
2024-09-06 12:34:30 -0500

    wip: XML Parse

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configuración
-CHECK_INTERVAL = 30  # Intervalo de chequeo en segundos
+CHECK_INTERVAL = 10  # Intervalo de chequeo en segundos
 IMAP_SERVER = "imap.gmail.com"
 EMAIL_ACCOUNT = os.getenv("EMAIL_ACCOUNT")
 PASSWORD = os.getenv("PASSWORD")
@@ -19,7 +19,7 @@ XLS_FILE = os.getenv("XLS_FILE")
 
 
 def check_for_new_emails():
-    # Conectarse al servidor IMAP
+    # Conexión al servidor IMAP
     mail = imaplib.IMAP4_SSL(IMAP_SERVER)
     mail.login(EMAIL_ACCOUNT, PASSWORD)
     mail.select("inbox")
@@ -31,7 +31,7 @@ def check_for_new_emails():
     for email_id in email_ids:
         _, msg_data = mail.fetch(email_id, "(RFC822)")
         msg = email.message_from_bytes(msg_data[0][1])
-        subject = msg["subject"]
+        subject = msg["subject"] if msg["subject"] else "Sin Asunto"
         print(f"Nuevo correo: {subject}")
 
         if msg.is_multipart():
@@ -39,31 +39,20 @@ def check_for_new_emails():
                 content_disposition = str(part.get("Content-Disposition"))
                 if "attachment" in content_disposition:
                     filename = part.get_filename()
-                    if filename:
-                        print(f"Archivo adjunto encontrado: {filename}")
+                    if filename and filename.endswith(".xml"):
                         # with open(filename, "wb") as f:
                         # f.write(part.get_payload(decode=True))
-                        if filename.endswith(".xml"):
-                            xml_content = (
-                                part.get_payload(decode=True)
-                                .decode("utf-8")
-                                .replace("&lt;", "<")
-                                .replace("&gt;", ">")
-                            )
-
-        mail.store(email_id, "-FLAGS", "\\Seen")  # Marcar como no leído
-
+                        xml_content = (
+                            part.get_payload(decode=True)
+                            .decode("utf-8")
+                            .replace("&lt;", "<")
+                            .replace("&gt;", ">")
+                        )
+                        # TODO: Parsear el contenido XML
+                        mail.store(email_id, "+FLAGS", "\\Seen")  # Marcar como leído
+                    else:
+                        mail.store(email_id, "-FLAGS", "\\Seen")  # Marcar como no leído
     mail.logout()
-
-
-def parse_xml_content(xml_content, root_tag, child_tag):
-    # Parsear el contenido XML
-    root = ET.fromstring(xml_content)
-    fact = root.find(".//%s" % root_tag)
-    if fact:
-        result = fact.find(".//%s" % child_tag)
-        return result.text
-    return None
 
 
 if __name__ == "__main__":
